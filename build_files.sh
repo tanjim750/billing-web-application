32f450bacc88a7551fd89039a6a4cab2b9236d1f
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2024-12-10 19:19:47 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,7 @@
 echo "BUILD START"
 # Install dependencies
 pip3 install -r requirements.txt
+python3 manage.py makemigrations
 python3 manage.py migrate
 # Run collectstatic to collect all static files
 python3 manage.py collectstatic --noinput --clear
