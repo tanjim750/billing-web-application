17df21153dbdaa517e9e15cdbd84a6afeda4d12f
tanjim750 <mdtanjim750@gmail.com>
2024-12-10 22:28:11 +0600

    added mysql remote database

--- billingApp/settings.py
+++ billingApp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wgaiqiwe8o78t9wv8p^cz&ct=hio848l!oj!afyqyx7%%8#yrp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["royalplazaesrarmarket.vercel.app",
                  "www.royalplazaesrarmarket.vercel.app",
                  ".vercel.app",
